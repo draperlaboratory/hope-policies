fb4df4ec40a76fcc5e047ebda0a88d2c242a61d7
Andrew Sutherland <andrew@dovermicrosystems.com>
2018-04-20 17:29:03 -0700

    Fix ping pong test

--- policy_tests/tests/ping_pong_works_1.c
+++ policy_tests/tests/ping_pong_works_1.c
@@ -1,10 +1,28 @@
 /*
- * Copyright © 2017-2018 Dover Microsystems, Inc.
+ * Copyright © 2017-2018 The Charles Stark Draper Laboratory, Inc. and/or Dover Microsystems, Inc.
  * All rights reserved. 
  *
- * Use and disclosure subject to license. 
+ * Use and disclosure subject to the following license. 
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-
 
 #include <string.h>
 #include <stdlib.h>
@@ -23,8 +41,12 @@
 #include "queue.h"
 #include "utils.h"
 
+/*
+ * Ping Pong tests makes sure that task switching works
+ */
+
 #define TICK_INTERVAL 2
-#define MSG_MAX 4
+#define MSG_MAX 6
 #define TIMER_INTERVAL (TICK_INTERVAL * configTICK_CLOCK_HZ / configTICK_RATE_HZ)
 
 #define PREEMPTIVE
@@ -41,16 +63,12 @@ static void done_task(void *p) {
   last_wake_time = xTaskGetTickCount();
   vTaskDelayUntil(&last_wake_time, TICK_INTERVAL);
 
-  
     while (1) {
         if(msg_count > MSG_MAX){
             test_done();
         }
         else {
-            uint64_t raw = xPortRawTime();
-            uint32_t low = (uint32_t)(raw & 0xFFFFFFFF);
-            uint32_t high = ((uint32_t)(raw >> 32)) & 0xFFFFFFFF;
-            printf_uart("Check if done: %x %x\r\n", high, low);
+            printf_uart("Check if done: %d < %d\r\n",msg_count, MSG_MAX);
         }
 #ifdef PREEMPTIVE  
         vTaskDelayUntil(&last_wake_time, TICK_INTERVAL);
@@ -67,7 +85,6 @@ static void ping_task(void *p) {
   (void)p;
   last_wake_time = xTaskGetTickCount();
   while (1) {
-      printf_uart("wake ping\r\n");
       if(pong == false){
           uint64_t raw = xPortRawTime();
           uint32_t low = (uint32_t)(raw & 0xFFFFFFFF);
@@ -90,7 +107,6 @@ static void pong_task(void *p) {
 
   last_wake_time = xTaskGetTickCount();
   while (1) {
-      printf_uart("wake pong\r\n");
       if(pong == true){
           uint64_t raw = xPortRawTime();
           uint32_t low = (uint32_t)(raw & 0xFFFFFFFF);
@@ -111,7 +127,8 @@ static void pong_task(void *p) {
 int test_main( void )
 {
     test_positive(); // identify test as positive (will complete)
-
+    test_begin();
+    
   /* no need to init uart */
   printf_uart("main: create ping task\r\n");
   xTaskCreate(ping_task, "Ping task", 1000, NULL, 1, NULL);
@@ -123,17 +140,16 @@ int test_main( void )
 
   printf_uart("timer ticks: 0x%x\r\n", TIMER_INTERVAL);
 
-  printf_uart("start scheduler\r\n");
-  vTaskStartScheduler();
+  // scheduler is already running so just wait
+  TickType_t last_wake_time;
   
-  // scheduler shouldn't get here unless a task calls xTaskEndScheduler()
+  last_wake_time = xTaskGetTickCount();
   for (;;){
-      for(volatile int i=0;i<10000;i++);
-      uint64_t raw = xPortRawTime();
-      uint32_t low = (uint32_t)(raw & 0xFFFFFFFF);
-      uint32_t high = ((uint32_t)(raw >> 32)) & 0xFFFFFFFF;
-      printf_uart("timer: %x %x\r\n", high, low);
-
+#ifdef PREEMPTIVE  
+      vTaskDelayUntil(&last_wake_time, TICK_INTERVAL);
+#else
+      taskYIELD();
+#endif
   }
 }
 
