b3a741b19eb59aa7cc7f5f97b6f4636a466521ba
Nathan Burow <nburow@draper.com>
2018-07-18 20:56:22 +0000

    new test cases

---
+++ policy_tests/tests/cpi/cpi_fails_1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "test_status.h"
+
+__attribute__((noinline))
+void func1(void){
+  t_printf("function 1\n");
+}
+
+__attribute__((noinline))
+void func2(void){
+  t_printf("function 2\n");
+}
+
+__attribute__((noinline))
+void sneaky(void *ptr){
+  memset(ptr, 0x0, 4);
+}
+
+int test_main(void){
+  test_negative(); // identify test as negative (will fail to complete due to policy violation)
+  test_begin();
+
+  void (*fptr)();
+  srand(0);
+  if(rand() % 2 == 0) fptr = func1;
+  else fptr = func2;
+
+  sneaky(&fptr);
+
+  fptr();
+
+  return test_done();
+}
