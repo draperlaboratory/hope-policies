47ef32ea8c09c91aafd534101b27810272220814
Nathan Burow <nburow@draper.com>
2018-07-19 21:01:32 +0000

    update threeClass policy to deal with nocfi - cfi transitions better

--- policy_tests/cfg_test.py
+++ policy_tests/cfg_test.py
@@ -4,13 +4,13 @@
 from cfg_classes import configs
 
 # Choose a test config from the classes defined in module testClasses
-cfg = 'working'
+cfg = 'llvm'
 
 # Which simulator to use for testing
-simulator = "renode"
+#simulator = "renode"
 #simulator = "verilator"
 #simulator = "fpga"
-#simulator = "qemu"
+simulator = "qemu"
 
 
 # Nothing to configure below here
