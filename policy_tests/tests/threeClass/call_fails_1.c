e68024b6c89b8512d72178e0bcc75ec3305b859e
Nathan Burow <nburow@draper.com>
2018-07-25 19:10:44 +0000

    debugged test case

--- policy_tests/tests/threeClass/call_fails_1.c
+++ policy_tests/tests/threeClass/call_fails_1.c
@@ -2,16 +2,15 @@
 #include "test_status.h"
 #include "test.h"
 
-__attribtue__((noinline))
-void target(void){
+__attribute__((noinline)) void target(void){
   t_printf("this is a target function\n");
   t_printf("canst thou hack it?\n");
   return;
 }
 
 int test_main(void){
-  size_t addr = (size_t)target + t;
-  void (*fptr)(void) = (void(*)(void))addr;
+  volatile size_t addr = (size_t)target + 4;
+  volatile void (*fptr)(void) = (void(*)(void))addr;
   test_negative();
   target();
   test_begin();
