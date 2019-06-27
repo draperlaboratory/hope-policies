a57b57760920c0e5def125817435319a4ff71b65
Scott Hadley <scott.hadley@dornerworks.com>
2019-06-27 08:11:16 -0400

    Limits: ported to test framework
    Change-Id: Icc6e0b44d3aaaad1ce563eefce9de46184b381a1

--- policy_tests/tests/limits/limit_test.c
+++ policy_tests/tests/limits/limit_test.c
@@ -1,4 +1,5 @@
-#include "../bareBench.h"
+#include "test_status.h"
+#include "test.h"
 
 extern unsigned short ult ( unsigned short a, unsigned short b);
 extern unsigned short slt ( short a, short b);
@@ -13,56 +14,54 @@ extern unsigned short ugte ( unsigned short a, unsigned short b);
 extern unsigned short sgte ( short a, short b);
 
 
-unsigned short limit_test ( void )
+int test_main(void)
 {
     unsigned short ra;
     short sa;
 
+    test_positive();
+    test_begin();
+    test_start_timer();
+
     for(ra=0;;ra++) if(ult(ra,7)==0) break;
-    if(ra!=7) return(1);
+    if(ra!=7) test_fail();
 
     for(ra=0xF000;;ra++) if(ult(ra,0xF007)==0) break;
-    if(ra!=0xF007) return(1);
+    if(ra!=0xF007) test_fail();
 
     for(sa=-7;;sa++) if(slt(sa,7)==0) break;
-    if(sa!=7) return(1);
+    if(sa!=7) test_fail();
 
     for(sa=-17;;sa++) if(slt(sa,-7)==0) break;
-    if(sa!=-7) return(1);
+    if(sa!=-7) test_fail();
 
     for(ra=0;;ra++) if(ulte(ra,7)==0) break;
-    if(ra!=8) return(1);
+    if(ra!=8) test_fail();
 
     for(ra=0xF000;;ra++) if(ulte(ra,0xF007)==0) break;
-    if(ra!=0xF008) return(1);
+    if(ra!=0xF008) test_fail();
 
     for(ra=0xF000;;ra++) if(ulte(ra,7)==0) break;
-    if(ra!=0xF000) return(1);
+    if(ra!=0xF000) test_fail();
 
     for(sa=-7;;sa++) if(slte(sa,7)==0) break;
-    if(sa!=8) return(1);
+    if(sa!=8) test_fail();
 
     for(sa=-17;;sa++) if(slte(sa,-7)==0) break;
-    if(sa!=-6) return(1);
+    if(sa!=-6) test_fail();
 
     for(ra=0;;ra++) if(ugt(ra,7)) break;
-    if(ra!=8) return(1);
+    if(ra!=8) test_fail();
 
     for(ra=0xF000;;ra++) if(ugt(ra,0xF007)) break;
-    if(ra!=0xF008) return(1);
+    if(ra!=0xF008) test_fail();
 
     for(ra=0;;ra++) if(ugte(ra,7)) break;
-    if(ra!=7) return(1);
+    if(ra!=7) test_fail();
 
     for(ra=0xF000;;ra++) if(ugte(ra,0xF007)) break;
-    if(ra!=0xF007) return(1);
+    if(ra!=0xF007) test_fail();
 
-    return(0);
-
+    test_print_total_time();
+    return test_done();
 }
-int main ( void )
-{
-    return(limit_test());
-}
-
-
