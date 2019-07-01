798a072e2a7887e0793c25fe8ca641a4b7b0c425
Scott Hadley <scott.hadley@dornerworks.com>
2019-07-01 12:55:02 -0400

    Randmath: ported to test framework
    Change-Id: Ic772758b3cece23bc486e4e0adf94e54e7a3ac0d

---
+++ policy_tests/tests/randmath/common.mk
@@ -0,0 +1,27 @@
+TEST_ROOT_DIR=..
+
+CFLAGS := $(ISP_CFLAGS)
+
+LDFLAGS += $(ISP_LDFLAGS)
+
+INCLUDES += $(ISP_INCLUDES)
+INCLUDES += -I$(TEST_ROOT_DIR)
+INCLUDES += -I$(TEST_ROOT_DIR)/randmath
+
+SOURCES := abcmath.c main.c
+SOURCES += $(TEST_ROOT_DIR)/test_status.c
+
+OBJECTS := $(patsubst %.c,%.o,$(SOURCES))
+
+TARGET := $(OUTPUT_DIR)/randmath
+
+all: $(TARGET)
+
+$(TARGET): $(ISP_OBJECTS) $(ISP_LIBS) $(ISP_DEPS) $(OBJECTS)
+	$(CC) $(CFLAGS) $(INCLUDES) $(ISP_LIBS) $(ISP_OBJECTS) \
+		$(OBJECTS) -o $@ $(LDFLAGS)
+
+include abcmath.mk
+
+$(OBJECTS): %.o: %.c $(SOURCES)
+	$(CC) $(CFLAGS) $(INCLUDES) $< -c -o $@
