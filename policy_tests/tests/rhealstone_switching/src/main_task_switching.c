ca3ef25d5be906d3f9d8db37a593b93c005b5502
Jesse Millwood <jesse.millwood@dornerworks.com>
2019-07-02 15:07:24 -0400

    Rhealstone: Ported task switching benchmark to test suite
    Change-Id: Ic4da40a2eb68d885c9718fb87f8aad6b8eb72934

--- policy_tests/tests/rhealstone_switching/src/main_task_switching.c
+++ policy_tests/tests/rhealstone_switching/src/main_task_switching.c
@@ -1,201 +1,156 @@
+/****************************************************************************/
+/* Author: Jesse Millwood                                                   */
+/* Date: 4/25/19                                                            */
+/* Task Switching Benchmark                                                 */
+/* OS: FreeRTOS                                                             */
+/* Plaform: RISC-V SiFIVE QEMU 3.14 Model                                   */
+/* Comments:                                                                */
+/* This is a mix between Timohty Boger's Master's Thesis Implementation for */
+/* the ZC702 and Daniel Ramirez's RTEMS implementation                      */
+/****************************************************************************/
 /*-----------------------------------------------------------
   Author: Timothy J Boger
-  Date: 4/29/13
-  Task Switching Benchmark
-  OS:FreeRTOS
+  Date: 4/29/13 Task Switching
+  Benchmark OS:FreeRTOS
   Platform: ZC702 Evaluation Board
-  References: - “FreeRTOS Port for Xilinx Zynq Devices” FreeRTOS Ltd. February 12, 2013.
-  - R. Kar.. "Implementing the Rhealstone Real-Time Benchmark". 1990.
-  - Cory Nakaji. "MIO, EMIO and AXI GPIO LEDS for ZC702". 2013.
+  References:
+   - “FreeRTOS Port for Xilinx Zynq Devices” FreeRTOS Ltd. February 12, 2013.
+   -  R. Kar.. "Implementing the Rhealstone Real-Time Benchmark". 1990.
+   -  Cory Nakaji. "MIO, EMIO and AXI GPIO LEDS for ZC702". 2013.
+  Ported to RISC-V and FreeRTOS 10 by DornerWorks' Jesse Millwood (2019)
 /*-----------------------------------------------------------*/
-// Includes
+
 #include "FreeRTOS.h"
+#include "rhealstone_utils.h"
 #include "task.h"
 #include "queue.h"
 #include "timers.h"
-#include "xil_printf.h"
 #include "stdio.h"
-#include "xparameters.h"
-#include "xgpio.h"
-#include "xgpiops.h"
-//**************************
-//AXI Variables
-static XGpioPs emio_pmod2;
-#define EMIO_54 54
-#define EMIO_55 55
-#define EMIO_56 56
-#define EMIO_57 57
-//**************************
+
 //Benchmark Variables
-#define MAX_LOOPS_SERIAL 500000 //Max loops for simulation
-#define MAX_LOOPS_TASK_SWITCHING 499999 //Accounting for extra Task3 switching
-unsigned long count1 = 0, count2 = 0;
+// #define BENCHMARK_LOOPS 500000
+#define BENCHMARK_LOOPS 100
+//Accounting for extra Task3 switching
+uint32_t count1 = 0;
+uint32_t count2 = 0;
 //*********************************************************
 // Priorities at which the tasks are created
 #define mainFIRST_TASK_PRIORITY  ( tskIDLE_PRIORITY + 2 )
 #define mainSECOND_TASK_PRIORITY ( tskIDLE_PRIORITY + 2 )
-#define mainTHIRD_TASK_PRIORITY  ( tskIDLE_PRIORITY + 3 )
+#define mainTHIRD_TASK_PRIORITY ( tskIDLE_PRIORITY + 1 )
+
 //*********************************************************
 //Associate Functions with Tasks
 static void prvFirst( void *pvParameters );
 static void prvSecond( void *pvParameters );
-static void prvThird( void *pvParameters );
+static void prvReport( void *pvParameters );
 //*********************************************************
 //Task Handle
 xTaskHandle xHandleFirst;
 xTaskHandle xHandleSecond;
-xTaskHandle xHandleThird;
+xTaskHandle xHandleReport;
+extern xTaskHandle xIspTask;
 //*********************************************************
-//Main
-int main( void )
+/* Test Record Keeping */
+mTaskSwitchTickRecord_t test_record;
+
+int test_main( void )
 {
-    prvInitializeExceptions();
-//*******************************************************
-//AXI Setup
-    XGpioPs_Config *ConfigPtrPS;
-    ConfigPtrPS = XGpioPs_LookupConfig(0);
-    XGpioPs_CfgInitialize(&emio_pmod2, ConfigPtrPS,
-                          ConfigPtrPS->BaseAddr);
-//*******************************************************
-//Setup PMOD 2 pins
-    XGpioPs_SetDirectionPin(&emio_pmod2, EMIO_54, 1);
-    XGpioPs_SetOutputEnablePin(&emio_pmod2, EMIO_54, 1);
-    XGpioPs_SetDirectionPin(&emio_pmod2, EMIO_55, 1);
-    XGpioPs_SetOutputEnablePin(&emio_pmod2, EMIO_55, 1);
-    XGpioPs_SetDirectionPin(&emio_pmod2, EMIO_56, 1);
-    XGpioPs_SetOutputEnablePin(&emio_pmod2, EMIO_56, 1);
-    XGpioPs_SetDirectionPin(&emio_pmod2, EMIO_57, 1);
-    XGpioPs_SetOutputEnablePin(&emio_pmod2, EMIO_57, 1);
-//*******************************************************
-//Setup PMOD 2 outputs to zero
-    XGpioPs_WritePin(&emio_pmod2, EMIO_54, 0x0);
-    XGpioPs_WritePin(&emio_pmod2, EMIO_55, 0x0);
-    XGpioPs_WritePin(&emio_pmod2, EMIO_56, 0x0);
-    XGpioPs_WritePin(&emio_pmod2, EMIO_57, 0x0);
-//*******************************************************
-//Start Benchmark
-    xil_printf("Start of Task Switching Benchmark\n\r");
-    xil_printf("Each task runs %D times\r\n", MAX_LOOPS_SERIAL);
-/*****************************************************************
-Serial Non_Switching Measurement
-Measure execution time of task1 and task2 when they are executed
-serially (without task switching).
-Measure the time between the High and Low GPIO output
-/******************************************************************/
-    xil_printf("Start Serial Non_Switching Measurement\r\n");
-    XGpioPs_WritePin(&emio_pmod2, EMIO_54, 0x1); //Set GPIO HIGH
-    for (count1 = 0; count1 < MAX_LOOPS_SERIAL; count1++)
+
+    vTaskPrioritySet(&xIspTask, tskIDLE_PRIORITY + 3);
+    test_positive();
+    test_begin();
+    /*****************************************************************
+     Serial Non_Switching Measurement Measure execution time of task1 and
+    task2 when they are executed serially (without task switching).
+    Measure the time between the High and Low GPIO output
+    /******************************************************************/
+    test_record.baseline_begin_ticks = get_timer_value();
+    for (count1 = 0; count1 < BENCHMARK_LOOPS; count1++)
     {
-//Do Nothing
+        //Do Nothing
     }
-    for (count2 = 0; count2 < MAX_LOOPS_SERIAL; count2++)
+    for (count2 = 0; count2 < BENCHMARK_LOOPS; count2++)
     {
-// Do Nothing
+        // Do Nothing
     }
-    XGpioPs_WritePin(&emio_pmod2, EMIO_54, 0x0); //Set GPIO LOW
-    xil_printf("Serial Non_Switching Measurement Done\r\n");
-/*****************************************************************
-Task Switching Measurement
-Create three tasks. Task 1 and Task 2 will perform the task switching.
-Task 3 controls the start and finish of the program and sets the GPIO pin
-Measure the time between the High and Low GPIO output
-******************************************************************/
-    xil_printf("Start Task Switching Measurement\r\n");
-//Create three tasks
-    xTaskCreate( prvFirst, ( signed char * ) "F",
-                 configMINIMAL_STACK_SIZE, NULL,
-                 mainFIRST_TASK_PRIORITY, &xHandleFirst );
-    xTaskCreate( prvSecond, ( signed char * ) "S",
-                 configMINIMAL_STACK_SIZE, NULL,
-                 mainSECOND_TASK_PRIORITY, &xHandleSecond );
-    xTaskCreate( prvThird, ( signed char * ) "T",
-                 configMINIMAL_STACK_SIZE, NULL,
-                 mainTHIRD_TASK_PRIORITY, &xHandleThird );
-    vTaskStartScheduler();
-/* If all is well, the scheduler will now be running, and the following line
-   will never be reached. If the following line does execute, then there was
-   insufficient FreeRTOS heap memory available for the idle and/or timer tasks
-   to be created. See the memory management section on the FreeRTOS web site
-   for more details. */
-    for( ;; );
+    test_record.baseline_end_ticks = get_timer_value();
+    t_printf("Done taking baseline measurement\n");
+    /*****************************************************************
+    Task Switching Measurement Create three tasks. Task 1 and Task 2 will
+    perform the task switching.
+
+    Task 3 controls the start and finish of the program and sets the
+    GPIO pin Measure the time between the High and Low GPIO output
+    ******************************************************************/
+
+    xTaskCreate( prvFirst, "Task 1", configMINIMAL_STACK_SIZE, NULL, mainFIRST_TASK_PRIORITY, &xHandleFirst  );
+    xTaskCreate( prvSecond, "Task 2", configMINIMAL_STACK_SIZE, NULL, mainSECOND_TASK_PRIORITY, &xHandleSecond  );
+    xTaskCreate( prvReport, "Report Task", configMINIMAL_STACK_SIZE, NULL, mainTHIRD_TASK_PRIORITY, &xHandleReport );
+
+    vTaskSuspend( xHandleReport);
+    taskYIELD();
 }
+
 //*********************************************************************
 //Task 3
-static void prvThird( void *pvParameters )
+static void prvReport( void *pvParameters )
 {
-    for( ;; )
+    uint32_t work_baseline_ticks;
+    uint32_t work_switching_ticks;
+    uint32_t switching_ticks;
+    uint32_t timer_freq;
+    uint32_t switching_time_usecs;
+
+    vTaskDelete(xHandleFirst);
+    vTaskDelete(xHandleSecond);
+
+    work_baseline_ticks = (test_record.baseline_end_ticks - test_record.baseline_begin_ticks);
+    work_switching_ticks = (test_record.measured_work_end_ticks - test_record.measured_work_begin_ticks);
+    if (work_baseline_ticks > work_switching_ticks)
     {
-//Runs First due to having highest priority
-        XGpioPs_WritePin(&emio_pmod2, EMIO_54, 0x1); //Set GPIO HIGH
-        vTaskPrioritySet(xHandleThird, tskIDLE_PRIORITY + 1);
-//reduce priority below Task 1 and 2
-//-------------------------- Task will yield here. Returns when Task 1 and 2 delete themselves
-//xil_printf("LOW\r\n");
-        XGpioPs_WritePin(&emio_pmod2, EMIO_54, 0x0); //Set GPIO LOW
-        xil_printf("Task Switching Measurement Done\r\n");
-        vTaskDelete(xHandleThird); //Delete Task 3
+        t_printf("ERROR: Ticks taken for baseline serial work is more than when using tasks\n");
+        t_printf("       The baseline should be less\n");
+        t_printf(" Baseline  : %lu\n", work_baseline_ticks);
+        t_printf(" Switching : %lu\n", work_switching_ticks);
     }
+    else
+    {
+        timer_freq = get_timer_freq();
+        // Average Ticks per task switch
+        switching_ticks = (work_switching_ticks - work_baseline_ticks)/((BENCHMARK_LOOPS *2) - 1);
+        switching_time_usecs = ticks_to_usecs(switching_ticks);
+        print_results_json("task_switching", "base", switching_ticks, timer_freq, switching_time_usecs);
+
+    }
+    vTaskResume(xIspTask);
+    test_pass();
+    test_done();
 }
 //*********************************************************************
 //Task 1
 static void prvFirst( void *pvParameters )
 {
-    for( ;; )
+    // Runs First
+    vTaskSuspend( xIspTask );
+    test_record.measured_work_begin_ticks = get_timer_value();
+    for (count1 = 0; count1 < BENCHMARK_LOOPS ; count1++)
     {
-        for (count1 = 0; count1 < MAX_LOOPS_TASK_SWITCHING;
-             count1++)
-        {
-            taskYIELD();
-        }
-        vTaskDelete(xHandleFirst); //Delete Task 1
+        taskYIELD();
     }
+    test_record.measured_work_end_ticks = get_timer_value();
+    vTaskPrioritySet(xHandleReport, tskIDLE_PRIORITY + 4);
+    vTaskResume(xHandleReport);
+    taskYIELD();
 }
 //*********************************************************************
 //Task 2
 static void prvSecond( void *pvParameters )
 {
-    for( ;; )
+    for (count2 = 0; count2 < BENCHMARK_LOOPS -1; count2++)
     {
-        for (count2 = 0; count2 < MAX_LOOPS_TASK_SWITCHING;
-             count2++)
-        {
-            taskYIELD();
-        }
-        vTaskDelete(xHandleSecond); //Delete Task 2
+        taskYIELD();
     }
+    taskYIELD();
 }
-//*********************************************************************
-void vApplicationMallocFailedHook( void )
-{
-/* vApplicationMallocFailedHook() will only be called if
-   configUSE_MALLOC_FAILED_HOOK is set to 1 in FreeRTOSConfig.h. It is a hook
-   function that will get called if a call to pvPortMalloc() fails.
-   pvPortMalloc() is called internally by the kernel whenever a task, queue or
-   semaphore is created. It is also called by various parts of the demo
-   application. If heap_1.c or heap_2.c are used, then the size of the heap
-   available to pvPortMalloc() is defined by configTOTAL_HEAP_SIZE in
-   FreeRTOSConfig.h, and the xPortGetFreeHeapSize() API function can be used
-   to query the size of free heap space that remains (although it does not
-   provide information on how the remaining heap might be fragmented). */
-    taskDISABLE_INTERRUPTS();
-    for( ;; );
-}
-//*********************************************************************
-void vApplicationStackOverflowHook( xTaskHandle *pxTask, signed char *pcTaskName )
-{
-    ( void ) pcTaskName;
-    ( void ) pxTask;
-/* vApplicationStackOverflowHook() will only be called if
-   configCHECK_FOR_STACK_OVERFLOW is set to either 1 or 2. The handle and name
-   of the offending task will be passed into the hook function via its
-   parameters. However, when a stack has overflowed, it is possible that the
-   parameters will have been corrupted, in which case the pxCurrentTCB variable
-   can be inspected directly. */
-    taskDISABLE_INTERRUPTS();
-    for( ;; );
-}
-//*********************************************************************
-void vApplicationSetupHardware( void )
-{
-/* Do nothing */
-}
+/*-----------------------------------------------------------*/
