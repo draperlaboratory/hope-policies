86139916448832fb82afcc46e388fa4a52626e3b
Jesse Millwood <jesse.millwood@dornerworks.com>
2019-07-09 14:20:00 -0400

    Stock Tests: Added support for stock bare and frtos runtimes
    Change-Id: Ib67951b4862ee902bbf158b6cb4591bc78852ec4

--- policy_tests/test_groups.py
+++ policy_tests/test_groups.py
@@ -22,8 +22,8 @@ class AllTests:
         "timer_works_1",
         "function_pointer_works_1",
         "function_pointer_works_2",
-        "rwx/code_write_fails_1", 
-        "rwx/data_exe_fails_1", 
+        "rwx/code_write_fails_1",
+        "rwx/data_exe_fails_1",
         "cfi/jump_data_fails_1",
         "heap/ptr_arith_fails_1",
         "heap/ptr_arith_fails_2",
@@ -95,6 +95,7 @@ class bare(AllTests):
                                  ]
                                  )]
 
+
 class Mibench(AllTests):
     tests = [
         "adpcm_decode",
@@ -116,10 +117,19 @@ class MibenchFrtos(Mibench):
                                  ]
                                  )]
 
+class stock_frtos(AllTests):
+    tests = frtos.tests
+
+class stock_bare(AllTests):
+    tests = bare.tests
+
+
 test_groups = {'all' : AllTests,
                'frtos' : frtos,
                'bare' : bare,
                'webapp' : webapp,
                'mibench' : Mibench,
                'mibenchfrtos' : MibenchFrtos,
+               'stock_frtos' : stock_frtos,
+               'stock_bare' : stock_bare
 }
