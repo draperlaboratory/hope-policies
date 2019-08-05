40c74229f357825c49e439e5c251b636c43651bc
Scott Hadley <scott.hadley@dornerworks.com>
2019-08-05 15:21:09 -0400

    Regress: port regress.s to RISC V
    Change-Id: I6d348c7b33a556a04b5582b4a63790fdc25525ff

--- policy_tests/tests/regress/regress.s
+++ policy_tests/tests/regress/regress.s
@@ -1,114 +1,110 @@
-
-;@-----------------------
-.thumb
-;@-----------------------
-.thumb_func
 .globl ROR
+.type ROR, @function
 ROR:
-    ror r0,r1
-    bx lr
+    addi sp,sp,-16
+    andi t1,a1,0xff     # ARM shifts by the least significant byte
+    neg t1,t1
+    sll t0,a0,t1
+    srl a0,a0,a1
+    or a0,a0,t0
+    addi sp,sp,16
+    ret
 
-.thumb_func
+
 .globl ROR_FLAG
+.type ROR_FLAG, @function
 ROR_FLAG:
-    ror r1,r2
-    mov r0,#0
-    mov r1,#1
-    bcs 1f
-    bcc 2f
-1:  orr r0,r1
-    b 3f
-2:  orr r0,r1
-3:
-    mov r1,#2
-    beq 1f
-    bne 2f
-1:  orr r0,r1
-    b 3f
-2:  orr r0,r1
-3:
-    mov r1,#4
-    bmi 1f
-    bpl 2f
-1:  orr r0,r1
-    b 3f
-2:  orr r0,r1
-3:
-    bx lr
+    addi sp,sp,-16
+    mv t0,a0
+    mv a0,x0            # Clear all flags
+    andi t1,a1,0xff     # ARM shifts by the least significant byte
+    beqz t1,SET_Z_FLAG  # If the shift value == 0, no changes are made
+    neg t2,t1
+    sll t3,t0,t2
+    srl t0,t0,t1
+    or t0,t0,t3
+    srl a0,t0,31        # Set carry flag if first bit is set
+    j SET_Z_FLAG
 
 
-.thumb_func
 .globl LSR
+.type LSR, @function
 LSR:
-    lsr r0,r1
-    bx lr
+    addi sp,sp,-16
+    andi t0,a1,0xff     # ARM shifts by the least significant byte
+    li t1,32
+    bgeu t0,t1,LSR1     # If the shift value >= 32, the result is cleared
+    srl a0,a0,t0
+    addi sp,sp,16
+    ret
+LSR1:
+    mv a0,x0
+    addi sp,sp,16
+    ret
 
 
-.thumb_func
 .globl LSR_FLAG
+.type LSR_FLAG, @function
 LSR_FLAG:
-    lsr r1,r2
-    mov r0,#0
-    mov r1,#1
-    bcs 1f
-    bcc 2f
-1:  orr r0,r1
-    b 3f
-2:  orr r0,r1
-3:
-    mov r1,#2
-    beq 1f
-    bne 2f
-1:  orr r0,r1
-    b 3f
-2:  orr r0,r1
-3:
-    mov r1,#4
-    bmi 1f
-    bpl 2f
-1:  orr r0,r1
-    b 3f
-2:  orr r0,r1
-3:
-    bx lr
+    addi sp,sp,-16
+    mv t0,a0
+    mv a0,x0            # Clear all flags
+    andi t1,a1,0xff     # ARM shifts by the least significant byte
+    li t2,32
+    # If the shift value > 32, the result is cleared and the carry bit is clear
+    bgtu t1,t2,CLEAR_RESULT
+    beqz t1,SET_Z_FLAG  # If the shift value == 0, no changes are made
 
-.thumb_func
+    addi t1,t1,-1
+    srl t0,t0,t1        # Perform all but the last shift
+    andi a0,t0,1        # Set carry flag if last bit is set
+    srl t0,t0,1         # Perform the last shift
+    j SET_Z_FLAG
+
+
 .globl LSL
+.type LSL, @function
 LSL:
-    lsl r0,r1
-    bx lr
+    addi sp,sp,-16
+    andi t0,a1,0xff     # ARM shifts by the least significant byte
+    li t1,32
+    bgeu t0,t1,LSL1     # If the shift value >= 32, the result is cleared
+    sll a0,a0,t0
+    addi sp,sp,16
+    ret
+LSL1:
+    mv a0,x0
+    addi sp,sp,16
+    ret
 
 
-.thumb_func
 .globl LSL_FLAG
+.type LSL_FLAG, @function
 LSL_FLAG:
-    lsl r1,r2
-    mov r0,#0
-    mov r1,#1
-    bcs 1f
-    bcc 2f
-1:  orr r0,r1
-    b 3f
-2:  orr r0,r1
-3:
-    mov r1,#2
-    beq 1f
-    bne 2f
-1:  orr r0,r1
-    b 3f
-2:  orr r0,r1
-3:
-    mov r1,#4
-    bmi 1f
-    bpl 2f
-1:  orr r0,r1
-    b 3f
-2:  orr r0,r1
-3:
-    bx lr
+    addi sp,sp,-16
+    mv t0,a0
+    mv a0,x0            # Clear all flags
+    andi t1,a1,0xff     # ARM shifts by the least significant byte
+    li t2,32
+    # If the shift value > 32, the result is cleared and the carry bit is clear
+    bgtu t1,t2,CLEAR_RESULT
+    beqz t1,SET_Z_FLAG  # If the shift value == 0, no changes are made
+
+    addi t1,t1,-1
+    sll t0,t0,t1        # Perform all but the last shift
+    srl a0,t0,31        # Set carry flag if first bit is set
+    sll t0,t0,1         # Perform the last shift
+    j SET_Z_FLAG
 
 
-
-;@-----------------------
-.end
-;@-----------------------
+CLEAR_RESULT:
+    mv t0,x0
+SET_Z_FLAG:
+    bne t0,x0,SET_N_FLAG
+    ori a0,a0,2
+SET_N_FLAG:
+    bgez t0,RETURN_FLAGS
+    ori a0,a0,4
+RETURN_FLAGS:
+    addi sp,sp,16
+    ret
