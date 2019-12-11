418617bccc2ad2f9f936e0d3510e17eb00316be1
John Merrill <jmerrill@draper.com>
2019-12-11 14:22:49 -0500

    Modify common.mk for freertos build changes

--- policy_tests/tests/adpcm_decode/common.mk
+++ policy_tests/tests/adpcm_decode/common.mk
@@ -22,7 +22,7 @@ TARGET := $(OUTPUT_DIR)/adpcm_decode
 all: $(TARGET)
 
 $(TARGET): $(ISP_OBJECTS) $(ISP_LIBS) $(ISP_DEPS) $(OBJECTS)
-	$(CC) $(CFLAGS) $(INCLUDES) $(ISP_LIBS) $(ISP_OBJECTS) \
+	$(CC) $(CFLAGS) $(INCLUDES) $(ISP_OBJECTS) \
 		$(OBJECTS) -o $@ $(LDFLAGS)
 
 $(OBJECTS): %.o: %.c $(SOURCES)
