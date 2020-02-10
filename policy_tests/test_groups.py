964643a6a6be166819f7ab103d0650a4628700b4
Brandon Butler <brandon.butler@dornerworks.com>
2020-02-10 16:09:06 -0500

    Testgen: Added testgen tests to test_groups.py
    Change-Id: Ic9cbae1de7661474e95cb61a1ed4a93a6f7c665c

--- policy_tests/test_groups.py
+++ policy_tests/test_groups.py
@@ -57,13 +57,137 @@ class AllTests:
         "qsort",
         "randmath",
         "lzfx",
-	"stringsearch",
-	"blowfish",
-	"limits",
-	"picojpeg",
+        "stringsearch",
+        "blowfish",
+        "limits",
+        "picojpeg",
         "coremark",
         "dhrystone",
     ]
+
+    testgenTests = [
+        "all-combined/hope-testgen-tests/7_NumericErrors/234/numeric_error_234",
+        "all-combined/hope-testgen-tests/7_NumericErrors/456p1/numeric_error_456p1",
+        "all-combined/hope-testgen-tests/7_NumericErrors/456p2/numeric_error_456p2",
+        "all-combined/hope-testgen-tests/7_NumericErrors/456p3/numeric_error_456p3",
+        "all-combined/hope-testgen-tests/7_NumericErrors/457p1/numeric_error_457p1",
+        "all-combined/hope-testgen-tests/7_NumericErrors/457p2/numeric_error_457p2",
+        "all-combined/hope-testgen-tests/7_NumericErrors/475/numeric_error_475",
+        "all-combined/hope-testgen-tests/7_NumericErrors/665p1/numeric_error_665p1",
+        "all-combined/hope-testgen-tests/7_NumericErrors/665p2/numeric_error_665p2",
+        "all-combined/hope-testgen-tests/7_NumericErrors/686/numeric_error_686",
+        "all-combined/hope-testgen-tests/7_NumericErrors/687/numeric_error_687",
+        "all-combined/hope-testgen-tests/7_NumericErrors/824p1/numeric_error_824p1",
+        "all-combined/hope-testgen-tests/7_NumericErrors/824p2/numeric_error_824p2",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/atoi_cached_biinterpreter",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/atoi_flatstore_biinterpreter",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/atoi_fragmented_biinterpreter",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/atoi_separate_biinterpreter",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/atoi_cached_simpleatoi",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/atoi_flatstore_simpleatoi",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/atoi_fragmented_simpleatoi",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/atoi_separate_simpleatoi",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/cache_cached_biinterpreter",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/cache_flatstore_biinterpreter",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/cache_fragmented_biinterpreter",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/cache_separate_biinterpreter",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/cache_cached_simpleatoi",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/cache_flatstore_simpleatoi",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/cache_fragmented_simpleatoi",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/cache_separate_simpleatoi",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/classifydeclassify_cached_biinterpreter",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/classifydeclassify_flatstore_biinterpreter",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/classifydeclassify_fragmented_biinterpreter",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/classifydeclassify_separate_biinterpreter",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/classifydeclassify_cached_simpleatoi",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/classifydeclassify_flatstore_simpleatoi",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/classifydeclassify_fragmented_simpleatoi",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/classifydeclassify_separate_simpleatoi",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/directsys_cached_biinterpreter",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/directsys_flatstore_biinterpreter",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/directsys_fragmented_biinterpreter",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/directsys_separate_biinterpreter",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/directsys_cached_simpleatoi",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/directsys_flatstore_simpleatoi",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/directsys_fragmented_simpleatoi",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/directsys_separate_simpleatoi",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/error_cached_biinterpreter",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/error_flatstore_biinterpreter",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/error_fragmented_biinterpreter",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/error_separate_biinterpreter",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/error_cached_simpleatoi",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/error_flatstore_simpleatoi",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/error_fragmented_simpleatoi",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/error_separate_simpleatoi",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/indexing_cached_biinterpreter",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/indexing_flatstore_biinterpreter",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/indexing_fragmented_biinterpreter",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/indexing_separate_biinterpreter",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/indexing_cached_simpleatoi",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/indexing_flatstore_simpleatoi",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/indexing_fragmented_simpleatoi",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/indexing_separate_simpleatoi",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/indexing2_cached_biinterpreter",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/indexing2_flatstore_biinterpreter",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/indexing2_fragmented_biinterpreter",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/indexing2_separate_biinterpreter",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/indexing2_cached_simpleatoi",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/indexing2_flatstore_simpleatoi",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/indexing2_fragmented_simpleatoi",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/indexing2_separate_simpleatoi",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/loginmsg_cached_biinterpreter",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/loginmsg_flatstore_biinterpreter",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/loginmsg_fragmented_biinterpreter",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/loginmsg_separate_biinterpreter",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/loginmsg_cached_simpleatoi",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/loginmsg_flatstore_simpleatoi",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/loginmsg_fragmented_simpleatoi",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/loginmsg_separate_simpleatoi",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/systemconfig_separate_simpleatoi",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/systemconfig_fragmented_simpleatoi",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/systemconfig_flatstore_simpleatoi",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/systemconfig_cached_simpleatoi",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/systemconfig_separate_biinterpreter",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/systemconfig_fragmented_biinterpreter",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/systemconfig_flatstore_biinterpreter",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/systemconfig_cached_biinterpreter",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/markprivate_separate_simpleatoi",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/markprivate_fragmented_simpleatoi",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/markprivate_flatstore_simpleatoi",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/markprivate_cached_simpleatoi",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/markprivate_separate_biinterpreter",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/markprivate_fragmented_biinterpreter",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/markprivate_flatstore_biinterpreter",
+        "all-combined/hope-testgen-tests/5_InformationLeakage/markprivate_cached_biinterpreter",
+        # "all-combined/hope-testgen-tests/1_BufferErrors/01", # Invalid Test
+        "all-combined/hope-testgen-tests/1_BufferErrors/02",
+        "all-combined/hope-testgen-tests/1_BufferErrors/03",
+        # "all-combined/hope-testgen-tests/1_BufferErrors/04", # Invalid Test
+        "all-combined/hope-testgen-tests/1_BufferErrors/05",
+        "all-combined/hope-testgen-tests/1_BufferErrors/06",
+        # "all-combined/hope-testgen-tests/1_BufferErrors/07", # Invalid Test
+        # "all-combined/hope-testgen-tests/1_BufferErrors/08", # Invalid Test
+        "all-combined/hope-testgen-tests/1_BufferErrors/09",
+        "all-combined/hope-testgen-tests/1_BufferErrors/10",
+        "all-combined/hope-testgen-tests/1_BufferErrors/11",
+        "all-combined/hope-testgen-tests/1_BufferErrors/12",
+        "all-combined/hope-testgen-tests/1_BufferErrors/13",
+        "all-combined/hope-testgen-tests/1_BufferErrors/14",
+        "all-combined/hope-testgen-tests/1_BufferErrors/15",
+        # "all-combined/hope-testgen-tests/1_BufferErrors/16", # Invalid Test
+        "all-combined/hope-testgen-tests/1_BufferErrors/18",
+        "all-combined/hope-testgen-tests/1_BufferErrors/17",
+        "all-combined/hope-testgen-tests/1_BufferErrors/19",
+        "all-combined/hope-testgen-tests/1_BufferErrors/20",
+        "all-combined/hope-testgen-tests/1_BufferErrors/21",
+        "all-combined/hope-testgen-tests/1_BufferErrors/22",
+        "all-combined/hope-testgen-tests/1_BufferErrors/23",
+        "all-combined/hope-testgen-tests/1_BufferErrors/24",
+        "all-combined/hope-testgen-tests/1_BufferErrors/25",
+    ]
+
+    # Tests that should be run only against all policies simultaneously.
+    combinedPolicyTests = testgenTests + []
 
 class webapp(AllTests):
     tests = [
@@ -78,27 +202,33 @@ class webapp(AllTests):
 
 
 class frtos(AllTests):
-    tests = [test for test in AllTests.tests
+    tests = [test for test in AllTests.tests + AllTests.combinedPolicyTests
                       if not any(test in s for s in
                                  [
                                   "heap-ppac-userType/webapp_patient_info_leak_fails",
                                   "password/webapp_password_leak",
-				  "dhrystone/dhrystone-baremetal",
                                   "fft",
                                   "timer_works_1",
+                                  "dhrystone/dhrystone-baremetal",
+                                  "all-combined/hope-testgen-tests/7_NumericErrors/456p1/numeric_error_456p1",
+                                  "all-combined/hope-testgen-tests/7_NumericErrors/456p2/numeric_error_456p2",
+                                  "all-combined/hope-testgen-tests/7_NumericErrors/456p3/numeric_error_456p3",
+                                  "all-combined/hope-testgen-tests/7_NumericErrors/457p1/numeric_error_457p1",
+                                  "all-combined/hope-testgen-tests/7_NumericErrors/457p2/numeric_error_457p2",
                                  ]
                                  )]
 
 
 class bare(AllTests):
-    tests = [test for test in AllTests.tests
+    tests = [test for test in AllTests.tests + AllTests.combinedPolicyTests
                       if not any(test in s for s in
                                  ["ping_pong_works_1",
                                   "dhrystone/dhrystone-baremetal",
-				  "hello_works_2",
+                                  "hello_works_2",
                                   "fft",
                                   "timer_works_1",
-                                 ]
+                                  "hello_works_2",
+                                 ] + AllTests.testgenTests
                                  )]
 
 test_groups = {'all' : AllTests,
