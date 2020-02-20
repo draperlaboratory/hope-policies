05b23d991825592f6418417cdc6b4136a10ed902
John Merrill <jmerrill@draper.com>
2020-02-20 12:07:26 -0500

    New heap tests for use after free, double free, and invalid pointer free

---
+++ policy_tests/tests/heap/use_after_free_fails_2.c
@@ -0,0 +1,63 @@
+/*
+ * Copyright © 2017-2018 The Charles Stark Draper Laboratory, Inc. and/or Dover Microsystems, Inc.
+ * All rights reserved. 
+ *
+ * Use and disclosure subject to the following license. 
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+#include "test_status.h"
+#include "test.h"
+
+#define HEAP_BUFFER_SIZE 42
+
+
+/*
+ * Test to check that a heap chunk cannot be read from after it has been freed.
+ */
+int test_main(void)
+{
+  char *heap_buffer;
+
+  test_negative();
+
+  heap_buffer = malloc(HEAP_BUFFER_SIZE);
+
+  printf("heap_buffer: %p\n", heap_buffer);
+  printf("*heap_buffer: %c\n", heap_buffer[0]); // demonstrate valid read from heap chunk
+
+  free(heap_buffer); 
+
+  test_begin(); // everything should work up to here
+
+  printf("*heap_buffer: %c\n", heap_buffer[0]); // invalid read from heap chunk, should fail
+
+  test_error("test_use_after_free_fails_2: Error this should not execute\n");  
+
+  return test_done();
+}
+
