a20c9ca42702f07d95ddac03b4b26fff6fd76829
Scott Hadley <Scott.Hadley@dornerworks.com>
2020-05-19 14:36:55 -0700

    Remove redundant info from webapp common.mk and update webapp test Makefiles
    Change-Id: Ib8bc446c49f2b3311009de6c568f061849420e61

--- policy_tests/tests/webapp/common.mk
+++ policy_tests/tests/webapp/common.mk
@@ -1,26 +1,5 @@
-CFLAGS += $(ISP_CFLAGS)
+TEST_HELPER_DIR = $(realpath ..)
+TEST_INCLUDES := -I$(TEST_HELPER_DIR) -I$(TEST_HELPER_DIR)/webapp
+TEST_SOURCES += $(wildcard $(TEST_HELPER_DIR)/webapp/*.c)
 
-INCLUDES += $(ISP_INCLUDES)
-INCLUDES += -I$(TEST_ROOT_DIR)
-INCLUDES += -I$(TEST_ROOT_DIR)/webapp
-
-LDFLAGS += $(ISP_LDFLAGS)
-
-SOURCES := $(wildcard *c)
-SOURCES := $(wildcard $(TEST_ROOT_DIR)/webapp/*.c)
-SOURCES += $(TEST_ROOT_DIR)/test_status.c
-SOURCES += $(TEST_ROOT_DIR)/test.c
-SOURCES += $(TEST_SOURCE)
-
-OBJECTS := $(patsubst %.c,%.o,$(SOURCES))
-
-TARGET := $(OUTPUT_DIR)/$(TEST)
-
-all: $(TARGET)
-
-$(TARGET): $(ISP_OBJECTS) $(ISP_LIBS) $(ISP_DEPS) $(OBJECTS)
-	$(CC) $(CFLAGS) $(INCLUDES) $(ISP_OBJECTS) \
-		$(OBJECTS) -o $@ $(LDFLAGS)
-
-$(OBJECTS): %.o: %.c $(SOURCES)
-	$(CC) $(CFLAGS) $(INCLUDES) $< -c -o $@
+include ../common.mk
