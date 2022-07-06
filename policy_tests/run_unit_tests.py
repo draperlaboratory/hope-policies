4bdd851a61a1bd5a5261791486c3552d10a0ed16
Alec Roelke <aroelke@draper.com>
2022-07-06 11:06:29 -0400

    Remove arch argument from run_unit_tests.py

--- policy_tests/run_unit_tests.py
+++ policy_tests/run_unit_tests.py
@@ -93,7 +93,7 @@ def test_new(test, runtime, policy, global_policy, sim, rule_cache, rule_cache_s
 def runTest(test, runtime, policy, pex, sim, rule_cache, rule_cache_size, output_dir, soc, timeout, arch, extra):
     global push_bitstream
     run_cmd = "isp_run_app"
-    run_args = [test, "-p", policy, "--pex", pex, "-s", sim, "-r", runtime, "-o", output_dir, "--arch", arch]
+    run_args = [test, "-p", policy, "--pex", pex, "-s", sim, "-r", runtime, "-o", output_dir]
     if rule_cache != "":
         run_args += ["-C", rule_cache, "-c", rule_cache_size]
 
